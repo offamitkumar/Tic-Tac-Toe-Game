3c89a419d254439256e1f36c48fcab2598d18c6b
Amit Kumar <offamitkumar@gmail.com>
2021-09-13 23:46:30 +0530

    test-multifiles

---
+++ functions.h
@@ -0,0 +1,23 @@
+
+/* functions Used */
+inline void                  clearSystem         ( void ) ;
+inline void                  startGame           ( void ) ;
+inline void                  developerInfo       ( void ) ;
+inline void                  printWarning        ( int  ) ;
+inline void                  welcomeUtility      ( void ) ;
+inline void                  goodByeMessage      ( void ) ;
+inline void                  play                ( void ) ;
+inline void                  setGridValues       ( void ) ;
+inline void                  printTheGrid        ( void ) ;
+inline bool                  isValid             ( int  ) ;
+inline void                  setGrid             ( void ) ;
+inline std::pair<int,int>    getGridLocation     ( int  ) ;
+inline bool                  isGameOver          ( void ) ;
+inline void                  printResult         ( void ) ;
+inline bool                  perfectName         ( void ) ;
+
+
+/* function to be added in future */
+void setGridSize     ( int  ) ;
+void printGrid       ( void ) ;
+void printResult     ( void ) ;
